d01a4c476de58da4d3bc8256d6e3630ad70b68e7
motazklef <motazklef@gmail.com>
2023-07-27 09:02:26 -0700

    ezo

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,17 @@
+			printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
+}
